bb07ad146ead88067df27b5002b7d46b04594067
SatyendraPrajapati456 <sp5267062@gmail.com>
2026-01-17 21:46:21 +0530

    Add files via upload

---
+++ Voice Assistant/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:voice_assistant/HomePage.dart';
+import 'package:voice_assistant/pallete.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Voice_Assistant',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+          elevation: 0,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
