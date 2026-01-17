749841244516c3dcd40fa4e328dc55c994943b33
SatyendraPrajapati456 <sp5267062@gmail.com>
2026-01-17 21:49:14 +0530

    Add files via upload

---
+++ Voice Assistant/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter_windows_3.38.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Coding Staf\\flutter\\Project\\voice_assistant" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter_windows_3.38.1-stable\\flutter"
+  "PROJECT_DIR=D:\\Coding Staf\\flutter\\Project\\voice_assistant"
+  "FLUTTER_ROOT=D:\\flutter_windows_3.38.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Coding Staf\\flutter\\Project\\voice_assistant\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Coding Staf\\flutter\\Project\\voice_assistant"
+  "FLUTTER_TARGET=D:\\Coding Staf\\flutter\\Project\\voice_assistant\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049M2I2MmVmYzJhMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzhmYzMwMTJlNA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC43"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Coding Staf\\flutter\\Project\\voice_assistant\\.dart_tool\\package_config.json"
+)
